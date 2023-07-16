83242e296a32d7954b00b116ab08fd856dd8247f
Kyle Levy <37563214+kylelevy@users.noreply.github.com>
2023-07-16 14:30:52 -0400

    Nevermind, not quite perfect.
    Added a meow method and a checkpoint method which both print to console as an indicator of where in the code you are.

--- bingo_bongo_cat.py
+++ bingo_bongo_cat.py
@@ -1,14 +1,26 @@
-def raise_error_with_cat(error_code):
-    error_message = """
-       /\_/\\
-      ( o.o )
-       > ^ <
-    """
-    if not type(error_code)==str:
-        error_code = str(error_code)
+import inspect
 
-    raise ValueError(f"Bingo Bongo, you have an error!\n\n{error_message}\nError Code: {error_code}")
+class FunnyCat:
+    def __init__(self):
+        self.cat = """
+        /\_/\\
+       ( o.o )
+        > ^ <
+        """
+
+    def meow(self):
+        print(f"\nBingo Bongo, meow!\n\n{self.cat}")
+        
+    def raise_error(self, error_code):
+        if not type(error_code)==str:
+            error_code = str(error_code)
+
+        raise ValueError(f"\nBingo Bongo, you have an error!\n\n{self.cat}\nError Code: {error_code}")
+    
+    def checkpoint(self):
+        line_num = inspect.currentframe().f_back.f_lineno
+        print(f"\nBingo Bongo, you have reached a checkpoint on line {line_num}!\n\n{self.cat}")
 
 # Calling the function to demonstrate the error
 if __name__ == '__main__':
-    raise_error_with_cat("Testing!")
+    FunnyCat().checkpoint()
