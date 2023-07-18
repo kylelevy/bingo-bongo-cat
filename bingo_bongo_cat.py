2308224907325bc0d15ba704fb1eef55991fd601
Kyle Levy <37563214+kylelevy@users.noreply.github.com>
2023-07-18 02:18:55 -0400

    Speech bubble
    Cause how can he talk without a speech bubble?!
    P.S. Multiline message functionality coming soon...

--- bingo_bongo_cat.py
+++ bingo_bongo_cat.py
@@ -2,25 +2,41 @@ import inspect
 
 class FunnyCat:
     def __init__(self):
-        self.cat = """
-        /\_/\\
+        self.cat = """        /\_/\\
        ( o.o )
         > ^ <
         """
 
     def meow(self):
-        print(f"\nBingo Bongo, meow!\n\n{self.cat}")
+        FunnyCat.say(self,"Bingo Bongo, meow")
         
     def raise_error(self, error_code):
         if not type(error_code)==str:
             error_code = str(error_code)
-
-        raise ValueError(f"\nBingo Bongo, you have an error!\n\n{self.cat}\nError Code: {error_code}")
+        error_msg = FunnyCat.say(self, f"Bingo Bongo, you have an error! Error Code: {error_code}", quiet=True)
+        raise ValueError(error_msg)
     
     def checkpoint(self):
         line_num = inspect.currentframe().f_back.f_lineno
-        print(f"\nBingo Bongo, you have reached a checkpoint on line {line_num}!\n\n{self.cat}")
+        FunnyCat.say(self, f"Bingo Bongo, you have reached a checkpoint on line {line_num}!")
+
+    def say(self, text, quiet=False):
+        if '\n' in text:
+            text = text.replace('\n','')
+        tb_line = ""
+        for char in range(len(text)):
+            tb_line += "━"
+        speechBubble = f"""
+            ┍━━{tb_line}━━┑
+            │  {text}  │
+            ┕━━{tb_line}━━┙
+               V"""
+        if quiet == False:
+            print(speechBubble)
+            print(self.cat)
+
+        return str(speechBubble+'\n'+self.cat)
 
 # Calling the function to demonstrate the error
 if __name__ == '__main__':
-    FunnyCat().checkpoint()
+    FunnyCat().say("Hi Y'all!")
